2ed73350aa734e95bbc2eeb95699f73614e931dd
Kuldeep <recs.cse1625@gmail.com>
2020-01-08 18:38:26 +0530

    init commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var todoController = require('./controllers/todocontrollers.js')
+
+var app = express();
+
+
+app.set('view engine','ejs');
+
+
+app.use(express.static('./public'));
+todoController(app);
+
+app.listen(3000);
+
+console.log('You are listening to the port 3000')
+
